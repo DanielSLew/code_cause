2a3e2d5e692c3bd7e5928cedd097de1290d0f1c9
Daniel Lew <danielshlomolew@gmail.com>
2020-07-18 14:41:39 -0400

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'projects#index'
+  root 'home#index'
 
-  resources :projects
+  namespace :api do
+    namespace :v1 do
+      resources :projects, only: [:index, :show, :create, :destroy]
+    end
+  end
+
+  # let React-router handle all requests that don't match the above routes
+  get '/*path' => 'home#index'
 end
